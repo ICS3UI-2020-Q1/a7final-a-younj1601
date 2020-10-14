9fa8ac409184ea8e0fbac983f503bbaef6160c97
younj1601 <younj1601@wrdsb.ca>
2020-10-14 19:46:13 +0000

    Finished the methods programming assignment

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
 /**
- *
- * @author 
+ *Counts the number of ones that appear in an integer
+ * @author Jackson
  */
 public class Main {
+
+  public static int countOnes(int num){
+    int count = 0;
+    while(num >= 10){
+      if(num % 10 == 1){
+        count = count + 1;
+      }
+      num = num / 10;
+    }
+    if(num == 1){
+      count = count + 1;
+    }
+    return count;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //
+    int count = countOnes(736482);
     
+    System.out.println(count);
   }
 }
